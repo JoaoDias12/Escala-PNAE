be584989f6b494c00fde11567ae21d723c9fe934
João Vitor Dias <104777038+JoaoDias12@users.noreply.github.com>
2025-03-08 16:25:03 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -78,7 +78,7 @@ let peoples = {
     Folga: false,
     Dupla: false,
     GateMaker: [5, 25],
-    Time: 'H1',
+    Time: 'H3',
     Count: [0, 0, 0, 0, 0],
     Folgas: [],
     Ferias: false
@@ -150,7 +150,7 @@ let peoples = {
     Folga: false,
     Dupla: false,
     GateMaker: [9, 16, 28],
-    Time: 'H0',
+    Time: 'H2',
     Count: [0, 0, 0, 0, 0],
     Folgas: [],
     Ferias: false
